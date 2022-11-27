f9c8726274f971274ddb4f39f24f9025d333f096
Henry Strömsten <henry.stromsten@corfima.se>
2022-11-27 12:45:16 +0000

    Navigate to Rexel website

--- uppgift2_selenium.py
+++ uppgift2_selenium.py
@@ -1,1 +1,11 @@
-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+'#open rexel website in chrome'
+driver.get("https://www.rexel.se")
+driver.fullscreen_window()
+time.sleep(4)
