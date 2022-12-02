f4386e16aa95ee15fe83c9262aa8d8d0edb58b77
Henry Strömsten <henry.stromsten@corfima.se>
2022-12-02 03:09:54 +0000

    Refactoring

--- calculator.py
+++ calculator.py
@@ -14,5 +14,8 @@ class calculate:
     def henrydiv(x, y):
         return x / y
 
-    def henryomkrets(x, y):
+    def henryomkretskvadrat(x, y):
         return 2*x + 2*y
+
+    def henryomkretscirkel(r):
+        return 2*r*3.14
--- test_calculator.py
+++ test_calculator.py
@@ -1,23 +1,28 @@
 import calculator
 
 def test_add():
-    result = calculator.calculate.henryadd(10, 15)
-    assert result == 25
+    #result = calculator.calculate.henryadd(10, 15)
+    #assert result == 25
+    assert calculator.calculate.henryadd(10,15) == 25
 
 def test_sub():
-    result = calculator.calculate.henrysub(10, 15)
-    assert result == -5
+    #result = calculator.calculate.henrysub(10, 15)
+    #assert result == -5
+    assert calculator.calculate.henrysub(25,15) == 10
+    assert calculator.calculate.henrysub(-20,-15) == -5
 
 def test_mult():
-    result = calculator.calculate.henrymult(10, 15)
-    assert result == 150
+    #result = calculator.calculate.henrymult(10, 15)
+    assert calculator.calculate.henrymult(10, 15) == 150
 
 def test_div():
-    result = calculator.calculate.henrydiv(30, 15)
-    assert result == 2
+    #result = calculator.calculate.henrydiv(30, 15)
+    assert calculator.calculate.henrydiv(30, 15) == 2
 
-def test_omkrets():
-    result = calculator.calculate.henryomkrets(20, 15)
-    assert result == 70
+def test_omkretskvadrat():
+    #result = calculator.calculate.henryomkretskvadrat(20, 15)
+    assert calculator.calculate.henryomkretskvadrat(20, 15) == 70
 
-
+def test_omkretscirkel():
+    #result = calculator.calculate.henryomkretscirkel(1)
+    assert calculator.calculate.henryomkretscirkel(1) == 6.28
