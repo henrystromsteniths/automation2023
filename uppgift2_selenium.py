f7856d25746e6b72b281f63c5a5236aca8a2984f
Henry Strömsten <henry.stromsten@corfima.se>
2022-12-04 18:35:06 +0000

    Added test functions - checking article numbers and adding one item to the basket.

--- uppgift2_selenium.py
+++ uppgift2_selenium.py
@@ -8,10 +8,30 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 '#open rexel website in chrome'
 driver.get("https://www.rexel.se")
 driver.fullscreen_window()
-time.sleep(4)
+time.sleep(2)
+driver.find_element(By.XPATH, "//*[@id='declineAllConsentSummary']").click()
 soki = driver.find_element(By.XPATH, '//*[@id="search"]')
 soki.send_keys('920021025303')
 soki.send_keys(Keys.ENTER)
 time.sleep(4)
+
+def test_kollaid():
+    item1 = driver.find_element(By.XPATH, "//*[@id='pdp-content-slot-1']/div[1]/div[7]/div[5]/div/div[4]/div/div[2]")
+    item1 = item1.text
+    assert "920021025303" in item1
+
+def test_kollaart():
+    item2 = driver.find_element(By.XPATH, "//*[@id='pdp-content-slot-1']/div[1]/div[7]/div[5]/div/div[3]/div/div[2]")
+    item2 = item2.text
+    assert "8340186" in item2
+
+
+def test_intobasket1():
+    driver.find_element(By.XPATH, "//*[@id='addToCartDiv_8340186']/span").click()
+    time.sleep(2)
+    basket1 = driver.find_element(By.XPATH, "//*[@id='miniCartContainer']/span")
+    basket1 = basket1.text
+    assert basket1 == "1"
+
 
 
